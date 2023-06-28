3a614bfa82ca6c09464dd6df94d8f8144fda6adf
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2023-06-28 10:42:00 +0530

    Update detectCycle.java

--- Problems on DFS and BFS/detectCycle.java
+++ Problems on DFS and BFS/detectCycle.java
@@ -1,58 +1,51 @@
-class Node {
-    int first;
-    int second;
-    public Node(int first, int second) {
-        this.first = first;
-        this.second = second; 
+class Pair{
+    int parent;
+    int child;
+    
+    Pair(int child , int parent ){
+        this.parent = parent;
+        this.child = child;
     }
 }
 class Solution {
-    // Function to detect cycle in an undirected graph.
-   static boolean checkForCycle(ArrayList<ArrayList<Integer>> adj, int s,
-            boolean vis[], int parent[])
-    {
-       Queue<Node> q =  new LinkedList<>(); //BFS
-       q.add(new Node(s, -1));
-       vis[s] =true;
-       
-       // until the queue is empty
-       while(!q.isEmpty())
-       {
-           // source node and its parent node
-           int node = q.peek().first;
-           int par = q.peek().second;
-           q.remove(); 
-           
-           // go to all the adjacent nodes
-           for(Integer it: adj.get(node))
-           {
-               if(vis[it]==false)  
-               {
-                   q.add(new Node(it, node));
-                   vis[it] = true; 
-               }
+    
+    public boolean bfs( int i , ArrayList<ArrayList<Integer>> adj , boolean vis[]){
         
-                // if adjacent node is visited and is not its own parent node
-               else if(par != it) return true;
-           }
-       }
-       
-       return false;
+            Queue<Pair> q = new LinkedList<>();    
+            
+            q.add(new Pair (i , -1));
+            vis[i] = true;
+            
+            while(!q.isEmpty()){
+                int parent = q.peek().parent;
+                int child = q.peek().child;
+                q.remove();
+                
+                for(int it : adj.get(child)){
+                    if(!vis[it]){
+                        
+                        q.add(new Pair (it , child));
+                        vis[it] = true;
+                        
+                    }else if(parent != it){ // if next node's edge is not its parent node
+                               return true;
+                    }
+                }
+            }
+            return false;
     }
     
-    // function to detect cycle in an undirected graph
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj)
-    {
-        boolean vis[] = new boolean[V];
-        Arrays.fill(vis,false);
-        int parent[] = new int[V];
-        Arrays.fill(parent,-1);  
-        
-        for(int i=0;i<V;i++)
-            if(vis[i]==false) 
-                if(checkForCycle(adj, i,vis, parent)) 
-                    return true;
     
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[] = new boolean[V];
+        
+        for(int i =0; i<V ; i++){
+            if(!vis[i])
+                if( bfs( i , adj , vis))
+                    return true;
+            
+        }
         return false;
     }
 }
