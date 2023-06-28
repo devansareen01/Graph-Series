43ad230084b6f45c0f828b146cd13f124c5e9039
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2023-06-28 11:01:44 +0530

    Update detectCycle.java

--- Problems on DFS and BFS/detectCycle.java
+++ Problems on DFS and BFS/detectCycle.java
@@ -27,7 +27,7 @@ class Solution {
                         q.add(new Pair (it , child));
                         vis[it] = true;
                         
-                    }else if(parent != it){ // if next node's edge is not its parent node
+                    }else if(parent != it){
                                return true;
                     }
                 }
@@ -35,14 +35,27 @@ class Solution {
             return false;
     }
     
-    
+    public boolean dfs(int i, int parent , ArrayList<ArrayList<Integer>> adj , boolean vis[]){
+        vis[i] = true; 
+        
+        for(int it : adj.get(i)){
+            if(!vis[it]){
+                if(dfs(it , i , adj , vis ) ){
+                    return true;
+                }
+            }else if( parent != it){
+                    return true;
+                }
+        }
+        return false;
+    }
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         boolean vis[] = new boolean[V];
         
         for(int i =0; i<V ; i++){
             if(!vis[i])
-                if( bfs( i , adj , vis))
+                if( dfs( i,-1 , adj , vis))
                     return true;
             
         }
