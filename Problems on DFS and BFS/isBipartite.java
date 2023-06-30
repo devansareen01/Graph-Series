5f2b6c4811dc0f0c772a3526b4cfdfa5ea46c291
Devan sareen <97303760+devansareen01@users.noreply.github.com>
2023-06-30 08:40:12 +0530

    Update isBipartite.java

--- Problems on DFS and BFS/isBipartite.java
+++ Problems on DFS and BFS/isBipartite.java
@@ -1,26 +1,33 @@
-class Solution {
-    public boolean isBipartite(int[][] graph) {
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-        boolean vis[] = new boolean[graph.length];
-        for(int i = 0; i<graph.length; i++){
-            if(!vis[i]){
-                if(!dfs(i, set1, set2, -1, graph, vis)) return false;
+class Solution
+{   
+    public boolean dfs(int color[] , ArrayList<ArrayList<Integer>>adj, int node , int currCol){
+        color[node] = currCol;
+        
+        for(int it : adj.get(node)){
+            if(color[it] == -1){
+                
+                if(dfs(color , adj , it ,1-currCol) == false)   return false;
+                
+                
+            }else if(color[it] == currCol){
+                return false;
             }
         }
         return true;
     }
-    private boolean dfs(int s, HashSet<Integer> set1, HashSet<Integer> set2, int par, int[][] graph, boolean vis[]){
-        if(set1.contains(par)) set2.add(s);
-        else set1.add(s);
-        vis[s] = true;
-        for(int v : graph[s]){
-            if(set1.contains(s)){
-                if(set1.contains(v)) return false;
-                else if(!vis[v] && !dfs(v, set1, set2, s, graph, vis)) return false;
-            } else {
-                if(set2.contains(v)) return false;
-                else if(!vis[v] && !dfs(v, set1, set2, s, graph, vis)) return false;
+    
+    
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        int color[] = new int[V];
+        
+        Arrays.fill(color , -1);
+        
+        for(int i =0; i<V; i++){
+            if(color[i] == -1){
+                if(!dfs( color , adj ,i, 0)){
+                     return false;
+                }
             }
         }
         return true;
